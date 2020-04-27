a69ba00b6f52426c990147a941a770e1344d3240
faizeansari <m.faizanyaseen92@gmail.com>
2020-04-28 01:19:57 +0500

    Odoo Testing Theme

---
+++ __manifest__.py
@@ -0,0 +1,16 @@
+{
+  'name': 'Tutorial theme',
+  'description': 'A description for your theme.',
+  'version': '1.0',
+  'author': 'Faize',
+
+  'data': [
+    'views/layout.xml',
+    'views/pages.xml',
+    'views/assets.xml',
+    'views/snippets.xml',
+    'views/options.xml'
+  ],
+  'category': 'Theme/Creative',
+  'depends': ['website', 'website_theme_install'],
+}
